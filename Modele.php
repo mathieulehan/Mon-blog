bd02434b7e4b566ef09d1aea33ab646a85c6783d
mathieulehan <mathieu.lehan@gmail.com>
2017-03-22 15:51:36 +0100

    billets

--- Modele.php
+++ Modele.php
@@ -16,4 +16,27 @@ $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8',
   'ts1', 'ts1', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   return $bdd;
 }
+
+// Renvoie les informations sur un billet
+function getBillet($idBillet) {
+	$bdd = getBdd();
+	$billet = $bdd->prepare('select BIL_ID as id, BIL_DATE as date,'
+			. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+			. ' where BIL_ID=?');
+	$billet->execute(array($idBillet));
+	if ($billet->rowCount() == 1)
+		return $billet->fetch();  // Accès à la première ligne de résultat
+		else
+			throw new Exception("Aucun billet ne correspond à l'identifiant '$idBillet'");
+}
+
+// Renvoie la liste des commentaires associés à un billet
+function getCommentaires($idBillet) {
+	$bdd = getBdd();
+	$commentaires = $bdd->prepare('select COM_ID as id, COM_DATE as date,'
+			. ' COM_AUTEUR as auteur, COM_CONTENU as contenu from T_COMMENTAIRE'
+			. ' where BIL_ID=?');
+	$commentaires->execute(array($idBillet));
+	return $commentaires;
+}
 ?>
