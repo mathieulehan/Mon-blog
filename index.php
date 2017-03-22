1fe432a43a9190493470c90dfaa344efe5ef05fc
mathieulehan <mathieu.lehan@gmail.com>
2017-03-22 14:37:19 +0100

    modele.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-<?php 
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'ts1', 'ts1');
-    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'. ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'. ' order by BIL_ID desc');
-    // Affichage
-    require 'vueAccueil.php';
-    ?>
+<?php
+
+require 'Modele.php';
+
+$billets = getBillets();
+
+require 'vueAccueil.php';
+
+?>
